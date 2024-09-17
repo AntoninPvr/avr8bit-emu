aeecac52ec8b9f647e4334f6cbb4e0a684d2a03b
Antonin <pivard.antonin@gmail.com>
2024-09-17 15:12:44 -0400

    added remaining data transfer function implementation (PC incrementation must be checked)

--- include/data_space.h
+++ include/data_space.h
@@ -4,6 +4,7 @@
 #include <stdbool.h>
 
 #define DATA_SPACE_SIZE 0x8FF
+#define PROGMEM_SPACE_SIZE 0x10000
 
 union DATA_SPACE {
     uint8_t data[DATA_SPACE_SIZE]; // too big for stack
@@ -13,4 +14,9 @@ union DATA_SPACE {
         uint8_t ext_io_reg[160];
         uint8_t sram[2048]; // not shared with SRAM
     };
+};
+
+union PROGMEM_SPACE {
+    uint8_t data[PROGMEM_SPACE_SIZE];
+    uint16_t word[PROGMEM_SPACE_SIZE / 2];
 };
--- src/data_transfer.c
+++ src/data_transfer.c
@@ -71,3 +71,512 @@ void ld_x(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
 
     inc_pc(core);
 }
+
+void ld_x_inc(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using X as address and post-increment X
+    // Rd <- (X)
+    // X <- X + 1
+    // 2 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->data[core->gp.X];
+    core->gp.X++;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ld_x_dec(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using X as address and post-decrement X
+    // X <- X - 1
+    // Rd <- (X)
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.X--;
+    core->gp.R[d] = data_space->data[core->gp.X];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ld_y(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using Y as address
+    // Rd <- (Y)
+    // 1 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->data[core->gp.Y];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ld_y_inc(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using Y as address and post-increment Y
+    // Rd <- (Y)
+    // Y <- Y + 1
+    // 2 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->data[core->gp.Y];
+    core->gp.Y++;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ld_y_dec(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using Y as address and post-decrement Y
+    // Y <- Y - 1
+    // Rd <- (Y)
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.Y--;
+    core->gp.R[d] = data_space->data[core->gp.Y];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ldd_y(uint8_t d, uint8_t q, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect with displacement from data space using Y as address
+    // Rd <- (Y + q)
+    // 2 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->data[core->gp.Y + q];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ld_z(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using Z as address
+    // Rd <- (Z)
+    // 1 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->data[core->gp.Z];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ld_z_inc(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using Z as address and post-increment Z
+    // Rd <- (Z)
+    // Z <- Z + 1
+    // 2 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->data[core->gp.Z];
+    core->gp.Z++;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ld_z_dec(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect from data space using Z as address and post-decrement Z
+    // Z <- Z - 1
+    // Rd <- (Z)
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.Z--;
+    core->gp.R[d] = data_space->data[core->gp.Z];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void ldd_z(uint8_t d, uint8_t q, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load indirect with displacement from data space using Z as address
+    // Rd <- (Z + q)
+    // 2 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->data[core->gp.Z + q];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void sts(int16_t k, uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store direct to data space
+    // k <- Rd
+    // 2 cycles
+
+    // Execute instruction
+    data_space->data[k] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_x(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using X as address
+    // (X) <- Rd
+    // 1 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.X] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_x_inc(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using X as address and post-increment X
+    // (X) <- Rd
+    // X <- X + 1
+    // 2 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.X] = core->gp.R[d];
+    core->gp.X++;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_x_dec(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using X as address and post-decrement X
+    // X <- X - 1
+    // (X) <- Rd
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.X--;
+    data_space->data[core->gp.X] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_y(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using Y as address
+    // (Y) <- Rd
+    // 1 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.Y] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_y_inc(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using Y as address and post-increment Y
+    // (Y) <- Rd
+    // Y <- Y + 1
+    // 2 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.Y] = core->gp.R[d];
+    core->gp.Y++;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_y_dec(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using Y as address and post-decrement Y
+    // Y <- Y - 1
+    // (Y) <- Rd
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.Y--;
+    data_space->data[core->gp.Y] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void std_y(uint8_t d, uint8_t q, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect with displacement to data space using Y as address
+    // (Y + q) <- Rd
+    // 2 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.Y + q] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_z(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using Z as address
+    // (Z) <- Rd
+    // 1 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.Z] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_z_inc(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using Z as address and post-increment Z
+    // (Z) <- Rd
+    // Z <- Z + 1
+    // 2 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.Z] = core->gp.R[d];
+    core->gp.Z++;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void st_z_dec(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect to data space using Z as address and post-decrement Z
+    // Z <- Z - 1
+    // (Z) <- Rd
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.Z--;
+    data_space->data[core->gp.Z] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void std_z(uint8_t d, uint8_t q, struct CORE *core, union DATA_SPACE *data_space) {
+    // Store indirect with displacement to data space using Z as address
+    // (Z + q) <- Rd
+    // 2 cycle
+
+    // Execute instruction
+    data_space->data[core->gp.Z + q] = core->gp.R[d];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void lpm_0(struct CORE *core, union PROGMEM_SPACE *progmem_space) {
+    // Load program memory to R0
+    // R0 <- Z
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.R0 = progmem_space->data[core->gp.Z];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void lpm_z(uint8_t d, struct CORE *core, union PROGMEM_SPACE *progmem_space) {    
+    // Load program memory to register using Z as address
+    // Rd <- (Z)
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.R[d] = progmem_space->data[core->gp.Z];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void lpm_z_inc(uint8_t d, struct CORE *core, union PROGMEM_SPACE *progmem_space) {
+    // Load program memory to register using Z as address and post-increment Z
+    // Rd <- (Z)
+    // Z <- Z + 1
+    // 3 cycle
+
+    // Execute instruction
+    core->gp.R[d] = progmem_space->data[core->gp.Z];
+    core->gp.Z++;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void in(uint8_t d, uint8_t A, struct CORE *core, union DATA_SPACE *data_space) {
+    // Read from I/O register
+    // Rd <- I/O[A]
+    // 1 cycle
+
+    // Execute instruction
+    core->gp.R[d] = data_space->io_reg[A];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void out(uint8_t A, uint8_t r, struct CORE *core, union DATA_SPACE *data_space) {
+    // Write to I/O register
+    // I/O[A] <- Rr
+    // 1 cycle
+
+    // Execute instruction
+    data_space->io_reg[A] = core->gp.R[r];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void push(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Push register to stack
+    // (SP) <- Rr
+    // SP <- SP - 1 
+    // 2 cycle
+
+    // Execute instruction
+    data_space->sram[core->SP] = core->gp.R[d];
+    core->SP--;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void pop(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Pop register from stack
+    // SP <- SP + 1
+    // Rd <- (SP)
+    // 2 cycle
+
+    // Execute instruction
+    core->SP++;
+    core->gp.R[d] = data_space->sram[core->SP];
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void xch(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Exchange register with stack
+    // Rd <-> (SP)
+    // 2 cycle
+
+    // Execute instruction
+    uint8_t temp = core->gp.R[d];
+    core->gp.R[d] = data_space->data[core->gp.Z];
+    data_space->data[core->gp.Z] = temp;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void las(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load and set register
+    // Only on SRAM
+    // In same time:
+    // (Z) <- Rd || (Z)
+    // Rd <- (Z)
+    // 2 cycle
+
+    // Execute instruction
+    uint8_t temp = data_space->data[core->gp.Z];
+    data_space->data[core->gp.Z] = core->gp.R[d] || temp;
+    core->gp.R[d] = temp;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void lac(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load and clear register
+    // Only on SRAM
+    // In same time:
+    // (Z) <- (0xFF - Rd) & (Z)
+    // Rd <- (Z)
+    // 2 cycle
+
+    // Execute instruction
+    uint8_t temp = data_space->data[core->gp.Z];
+    data_space->data[core->gp.Z] = (0xFF - core->gp.R[d]) & temp;
+    core->gp.R[d] = temp;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
+
+void lat(uint8_t d, struct CORE *core, union DATA_SPACE *data_space) {
+    // Load and toggle register
+    // Only on SRAM
+    // In same time:
+    // (Z) <- Rd ^ (Z)
+    // Rd <- (Z)
+    // 2 cycle
+
+    // Execute instruction
+    uint8_t temp = data_space->data[core->gp.Z];
+    data_space->data[core->gp.Z] = core->gp.R[d] ^ temp;
+    core->gp.R[d] = temp;
+
+    // Update SREG
+    // NONE
+
+    inc_pc(core);
+}
