4a3262dce7b3ed228f6cc3b27ed286eefb39eb2f
Antonin <pivard.antonin@gmail.com>
2024-09-17 19:09:44 -0400

    added branch function declaration

--- include/branch.h
+++ include/branch.h
@@ -4,9 +4,41 @@
 
 #include "core.h"
 #include "sram.h"
+#include "data_space.h"
 
 void rjmp(uint8_t k, struct CORE *core);
 void ijmp(struct CORE *core);
 void jmp(uint32_t k, struct CORE *core);
 void rcall(uint8_t k, struct CORE *core, struct SRAM *sram);
 void icall(struct CORE *core, struct SRAM *sram);
+void eicall(struct CORE *core, struct SRAM *sram);
+void ret(struct CORE *core, struct SRAM *sram);
+void reti(struct CORE *core, struct SRAM *sram);
+void cpse(uint8_t d, uint8_t r, struct CORE *core);
+void cp(uint8_t d, uint8_t r, struct CORE *core);
+void cpc(uint8_t d, uint8_t r, struct CORE *core);
+void cpi(uint8_t d, uint8_t K, struct CORE *core);
+void sbrc(uint8_t r, uint8_t b, struct CORE *core);
+void sbrs(uint8_t r, uint8_t b, struct CORE *core);
+void sbic(uint8_t A, uint8_t b, struct CORE *core, union DATA_SPACE *data_space);
+void sbis(uint8_t A, uint8_t b, struct CORE *core, union DATA_SPACE *data_space);
+void brbs(uint8_t s, uint8_t k, struct CORE *core);
+void brbc(uint8_t s, uint8_t k, struct CORE *core);
+void breq(uint8_t k, struct CORE *core);
+void brne(uint8_t k, struct CORE *core);
+void brcs(uint8_t k, struct CORE *core);
+void brcc(uint8_t k, struct CORE *core);
+void brsh(uint8_t k, struct CORE *core);
+void brlo(uint8_t k, struct CORE *core);
+void brmi(uint8_t k, struct CORE *core);
+void brpl(uint8_t k, struct CORE *core);
+void brge(uint8_t k, struct CORE *core);
+void brlt(uint8_t k, struct CORE *core);
+void brhs(uint8_t k, struct CORE *core);
+void brhc(uint8_t k, struct CORE *core);
+void brts(uint8_t k, struct CORE *core);
+void brtc(uint8_t k, struct CORE *core);
+void brvs(uint8_t k, struct CORE *core);
+void brvc(uint8_t k, struct CORE *core);
+void brie(uint8_t k, struct CORE *core);
+void brid(uint8_t k, struct CORE *core);
--- src/branch.c
+++ src/branch.c
@@ -5,7 +5,6 @@ void rjmp(uint8_t k, struct CORE *core) {
     // PC <- PC + k + 1
     // 2 cycles
     core->PC = core->PC + k + 1;
-    return 0;
 }
 
 void ijmp(struct CORE *core) {
@@ -13,7 +12,6 @@ void ijmp(struct CORE *core) {
     // PC <- Z
     // 2 cycles
     core->PC = core->gp.R[30] << 8 | core->gp.R[31];
-    return 0;
 }
 
 void jmp(uint32_t k, struct CORE *core) {
@@ -21,7 +19,6 @@ void jmp(uint32_t k, struct CORE *core) {
     // PC <- k
     // 3 cycles
     core->PC = k;
-    return 0;
 }
 
 void rcall(uint8_t k, struct CORE *core, struct SRAM *sram) {
@@ -32,5 +29,4 @@ void rcall(uint8_t k, struct CORE *core, struct SRAM *sram) {
     sram->mem[core->SP] = core->PC + 1;
     core->SP - 2;
     core->PC = core->PC + k + 1;
-    return 0;
 }
